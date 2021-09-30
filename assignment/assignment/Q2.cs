56fe84495cc1434275b9fdc32a0060fdfa4e26a5
harish <hellookya@gmail.com>
2021-09-30 14:25:47 +0530

    all the files are added

---
+++ assignment/assignment/Q2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    class Q2
+    {
+		public static void Main()
+		{
+			int n;
+			Console.Write("enter the day:");
+			n = int.Parse(Console.ReadLine());
+			if (n > 0 && n <= 7)
+			{
+				if (n > 0 && n < 6)
+					Console.WriteLine("working day");
+				else
+					Console.WriteLine("holiday");
+			}
+			else
+				Console.WriteLine("invalid input");
+		}
+
+	}
+}
